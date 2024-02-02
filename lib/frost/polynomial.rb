0e3f30bc19fa0254b3313feb04a666f714543ee3
azuchi <azuchi@chaintope.com>
2024-02-02 16:56:30 +0900

    Add min degree validation

--- lib/frost/polynomial.rb
+++ lib/frost/polynomial.rb
@@ -8,6 +8,8 @@ module FROST
     def initialize(coefficients, group)
       raise ArgumentError, "coefficients must be an Array." unless coefficients.is_a?(Array)
       raise ArgumentError, "group must be ECDSA::Group." unless group.is_a?(ECDSA::Group)
+      raise ArgumentError, "Two or more coefficients are required." if coefficients.length < 2
+
       @coefficients = coefficients
       @group = group
     end
@@ -19,7 +21,7 @@ module FROST
     def self.from_secret(secret, degree, group)
       raise ArgumentError, "secret must be Integer." unless secret.is_a?(Integer)
       raise ArgumentError, "degree must be Integer." unless degree.is_a?(Integer)
-      raise ArgumentError, "degree must be greater than or equal to 2" if degree < 2
+      raise ArgumentError, "degree must be greater than or equal to 1." if degree < 1
 
       coeffs = degree.times.map {SecureRandom.random_number(group.order - 1)}
       Polynomial.new(coeffs << secret, group)
--- spec/polynomial_spec.rb
+++ spec/polynomial_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 RSpec.describe FROST::Polynomial do
 
+  let(:group) { ECDSA::Group::Secp256k1 }
+
   describe "#from_secret" do
-    let(:group) { ECDSA::Group::Secp256k1 }
     let(:secret) { SecureRandom.random_number(group.order - 1) }
     it do
       poly = described_class.from_secret(secret, 3, group)
@@ -13,10 +14,22 @@ RSpec.describe FROST::Polynomial do
 
     context "invalid degree" do
       it do
-        expect{described_class.from_secret(secret, 1, group)}.
-          to raise_error(ArgumentError, "degree must be greater than or equal to 2")
+        expect{described_class.from_secret(secret, 0, group)}.
+          to raise_error(ArgumentError, "degree must be greater than or equal to 1.")
       end
     end
   end
 
+  describe "#new" do
+    it do
+      expect{described_class.new([1, 2], group)}.not_to raise_error
+    end
+
+    context "degree less than 1" do
+      it do
+        expect{described_class.new([1], group)}.
+          to raise_error(ArgumentError, "Two or more coefficients are required.")
+      end
+    end
+  end
 end
